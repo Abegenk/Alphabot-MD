c10a3cc683969e35c0dcc13e318bc66441b17eba
Abegenk <102190428+Abegenk@users.noreply.github.com>
2022-04-14 03:27:16 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -40,8 +40,8 @@ global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput
 global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 
 global.prefa = ['!','.','*','#']
-global.sp = '⭔'
-global.mess = 
+global.sp = '.'
+global.mess = {
     sukses: '🤗Done, Oke Bree~',
     admin: 'Perintah Ini Hanya Bisa Digunakan Oleh Admin !',
     botAdmin: 'Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin !',
