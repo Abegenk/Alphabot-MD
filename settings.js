3f5e4b7a6e9252e656d67058f48e9ea1ab34c9de
Abegenk <102190428+Abegenk@users.noreply.github.com>
2022-04-14 03:04:26 +0700

    Add files via upload

--- settings.js
+++ settings.js
@@ -39,8 +39,8 @@ global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutpu
 global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 
-global.prefa = ['/','!','.','🐦','🐤','🗿']
-global.sp = '⭔'
+global.prefa = [','!','.']
+global.sp = '.'
 global.mess = {
     sukses: '🤗Done, Oke Bree~',
     admin: 'Perintah Ini Hanya Bisa Digunakan Oleh Admin !',
