3b489d8df3191daf0d68c9fe48effa850e8e012b
Abegenk <102190428+Abegenk@users.noreply.github.com>
2022-07-22 09:54:58 +0800

    Add files via upload

--- settings.js
+++ settings.js
@@ -1,48 +1,63 @@
 const fs = require('fs')
 const chalk = require('chalk')
+const  { indonesia, english, spanyol} = require(`./language`)
 
 // Website Api
 global.APIs = {
-	alfa: 'https://api-mlsidbot.herokuapp.com',
+	alfa: 'https://api.zeeoneofc.xyz', //apabila link api eror, segera laporkan ke owner
 }
 
 // Free apikey
 global.APIKeys = {
-	'https://api-mlsidbot.herokuapp.com': 'MlsidBot',
+	'https://api.zeeoneofc.xyz': 'V12',  // login https://api.zeeoneofc.xyz to get apikey || https://api-alphabot.herokuapp.com
 }
 
 // setting 
 global.autoread = false // auto read pesan / message
-global.autorecording = true //status auto merekam ( auto record )
-global.autoketik = false //status auto mengetik (auto typing)
-global.available = false //status online (online)
+global.autobio = true 
+
+//language
+//Available in indonesia , english & spanyol 
+//You can request others language 
+global.language = indonesia  //change indonesia to english if you don't understand the language used by the bot
+
 
 // Other
+// Other
 global.botname = "MlsidBotོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ A b e e yོ ×፝֟͜×" //nama kalian
-global.myweb ="https://api-MlsidBot.herokuapp.com/" //bebas asal jan hapus
-global.youtube = "https://youtube.com/channel/AFKSQUAD" //bebas asal jan hapus
-global.github = "https://github.com/abegenk" //bebas
-global.email = "abilstoy@gmail.com" //bebas
+global.ownername= "ᴹᴿ᭄ A b e e Yོ ×፝֟͜×" //nama kalian
+global.myweb ="https://api.abegenk.xyz" //bebas asal jan hapus
+global.youtube = "https://youtube.com/c/AfkSquad" //bebas asal jan hapus
+global.github = "https://abegenk.github.io/" //bebas
+global.email = "afksquad.nad@gmail.com" //bebas
 global.region = "Indonesia" //bebas
+global.timezone = 'Asia/Jakarta' //  timezone wib
+global.premium = ['6289510437264'] //premium user
+global.owner = ["6289510437264"] //ganti agar fitur owner bisa di gunakan
 global.ownernomer = "6289510437264" // nomor wa kalian
 global.ownernomerr = "+6289510437264" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
-global.background_welcome="https://telegra.ph/file/Foto1.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.owner = ["6289510437264"] //ganti agar fitur owner bisa di gunakan
-global.packname = '© MlsidBotོ' //sticker wm ubah
-global.author = 'Di Buat Oleh A b e e y' //sticker wm ganti nama kalian
+global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
+global.packname = '© Mlsid - BOTོ' //sticker wm ubah
+global.author = 'Di Buat Oleh A b e e Y' //sticker wm ganti nama kalian
 global.sessionName = 'session'
+global.typemenu = 'document'
+
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 
-global.prefa = ['!','.','*','#']
-global.sp = '.'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
 global.mess = {
-    sukses: '🤗Done, Oke Bree~',
+    sukses: '🤗Done, Oke Desu~',
     admin: 'Perintah Ini Hanya Bisa Digunakan Oleh Admin !',
     botAdmin: 'Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin !',
     owner: 'Perintah Ini Hanya Bisa Digunakan Oleh Owner !',
